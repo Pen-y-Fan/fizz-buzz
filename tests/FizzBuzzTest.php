7fbdcb50f062ed2f23a3fb142041a3752d8091f7
Pen-y-Fan <40126936+Pen-y-Fan@users.noreply.github.com>
2020-06-11 22:47:52 +0100

    First test

---
+++ tests/FizzBuzzTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+final class FizzBuzzTest extends TestCase
+{
+    public function testTrue(): void
+    {
+        $this->assertTrue(true);
+    }
+}
